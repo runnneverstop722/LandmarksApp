8111959715e8b5c0925117852d4ca8fa04076dd2
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-18 11:18:07 +0900

    Changed Hierarchy. Add

---
+++ LandmarksApp/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  LandmarksApp
+//
+//  Created by Teff on 2023/05/18.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "Star.fill" : "Star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
