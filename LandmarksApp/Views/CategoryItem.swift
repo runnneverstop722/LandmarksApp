910325d21406e8ed05713f278d325e027015d42d
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-19 23:43:32 +0900

    - Add Navigation Between Sections : With all of the differently categorized landmarks visible in the view, the user needs a way to reach each section in the app. Use the navigation and presentation APIs to make the category home, the detail view, and favorites list navigable from a tab view.

--- LandmarksApp/Views/CategoryItem.swift
+++ LandmarksApp/Views/CategoryItem.swift
@@ -13,10 +13,12 @@ struct CategoryItem: View {
     var body: some View {
         VStack(alignment: .leading) {
             landmark.image
+                .renderingMode(.original) // Text that you pass as the label for a navigation link renders using the environment’s accent color, and images may render as template images. You can modify either behavior to best suit your design.
                 .resizable()
                 .frame(width: 155, height: 155)
                 .cornerRadius(5)
             Text(landmark.name)
+                .foregroundColor(.primary) // Text that you pass as the label for a navigation link renders using the environment’s accent color, and images may render as template images. You can modify either behavior to best suit your design.
                 .font(.caption)
         }
         .padding(.leading, 15)
--- LandmarksApp/Views/ContentView.swift
+++ LandmarksApp/Views/ContentView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured // Add a state variable for the tab selection, and give it a default value.
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        //The tag(_:) modifier on each of the views matches one of the possible values that the selection property can take so the TabView can coordinate which view to display when the user makes a selection in the user interface.
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem({
+                    Label("Featured", systemImage: "star")
+                })
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem({
+                    Label("List", systemImage: "list.bullet")
+                })
+                .tag(Tab.list)
+        }
     }
 }
 
